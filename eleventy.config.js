b14bbbbe746a090b95c95532c14a2cc58026fce6
Leia Chang <leia.chang@exploratorium.edu>
2025-07-03 11:05:03 -0700

    add htmlbase to fix pathing issues

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,13 @@
 const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 
+
 module.exports = async function(eleventyConfig) {
 	const { EleventyRenderPlugin } = await import("@11ty/eleventy");
+	const { HtmlBasePlugin } = await import("@11ty/eleventy");
+
   eleventyConfig.addPlugin(EleventyRenderPlugin);
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
 
